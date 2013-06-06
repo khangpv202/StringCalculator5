295bb5bed9be12cf7d15cb6eb31df5f718cdb75b
unknown <khangpv@qs089-PC.qsoft.com.vn>
2013-06-06 13:39:59 +0700

    6/6 StringCalculator
    Signed-off-by: unknown <khangpv@qs089-PC.qsoft.com.vn>

---
+++ src/main/java/Calculator/Calculator.java
@@ -0,0 +1,14 @@
+package Calculator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: khangpv
+ * Date: 6/6/13
+ * Time: 1:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Calculator {
+    public int add(String s) {
+        return 0;  //To change body of created methods use File | Settings | File Templates.
+    }
+}
---
+++ src/test/java/TestStringCalculator.java
@@ -0,0 +1,19 @@
+import Calculator.Calculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: khangpv
+ * Date: 6/6/13
+ * Time: 1:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestStringCalculator {
+    @Test
+    public void testEmptyString(){
+        Calculator cal = new Calculator();
+        assertEquals(0,cal.add(""));
+    }
+}
